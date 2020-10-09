8a64d791efd6e7343f5a77096beb0cf69dd044b4
Florian <florian.grember@gmail.com>
2020-10-09 10:28:00 +0200

    test

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,11 +7,12 @@ import {StopEntity} from "../modules/stop/stop.entity";
 @Module({
     imports: [TypeOrmModule.forRoot({
         type:'postgres',
-        host:process.env.HEROKU_DB_HOST || 'localhost',
-        port: parseInt(process.env.HEROKU_DB_PORT) || 5432,
-        username:process.env.HEROKU_DB_USERNAME ||'postgres',
-        password:process.env.HEROKU_DB_PASSWORD ||'root',
-        database:process.env.HEROKU_DB_NAME || 'project-bus',
+        url:process.env.DATABASE_URL,
+        // host:process.env.HEROKU_DB_HOST || 'localhost',
+        // port: parseInt(process.env.HEROKU_DB_PORT) || 5432,
+        // username:process.env.HEROKU_DB_USERNAME ||'postgres',
+        // password:process.env.HEROKU_DB_PASSWORD ||'root',
+        // database:process.env.HEROKU_DB_NAME || 'project-bus',
         entities:[ChildrenEntity,SchoolEntity,StopEntity],
         synchronize:true
     })]
