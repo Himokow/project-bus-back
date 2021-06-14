8867e934124b952591d0a72ccc3da76b6b58bb0f
Himokow <67366531+Himokow@users.noreply.github.com>
2021-06-14 08:07:08 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,11 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors();
+  app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"),
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
+  next(),
+}),
   await app.listen(process.env.PORT || 3000);
   Logger.log(`🚀 Server running on http://localhost:${port}`, 'Bootstrap');
 
